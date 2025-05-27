01bfa052e087dc7e79617a51bdfa3db07e153a7d
AlfarAbu <raflasalfa@gmail.com>
2025-05-27 22:19:52 +0530

    creation of basic portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,52 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+<main className="flex flex-wrap justify-center w-full h-fit text-center items-center bg-amber-100">
+  <div className="w-full h-full md:w-1/2 p-3 flex flex-col items-center">
+    <Image
+      src="/profile.jpg"
+      alt="Profile"
+      width={150}
+      height={150}
+      className="border-s-2 rounded-full mb-4 "
+    />
+    <h1 className="text-3xl font-bold text-amber-950 mb-2">
+      Hi 👋, I'm Alfar Abusalihu
+    </h1>
+        <p className="text-lg max-w-xl text-gray-700 mb-6">
+          A tech enthusiast passionate about rapidly evolving technologies. As a quick learner, I easily adapt to
+          different tech stacks and tools. My goal is to contribute to my workplace's continuous growth while
+          enhancing my skills and experience to the next level
+        </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex p-2">
+          <p className="mx-2 space-y-2 items-center flex">
+            <a href="mailto:alfarabusalihu@gmail.com" className="hover:underline block"><Image src="/gmail.jpg" className="rounded-2xl border-b-black" alt="logo" width={40} height={40}></Image></a><br />
+          </p>
+          <p className=" mx-2 space-y-2 items-center flex">
+            <a href="https://linkedin.com/in/your-profile" target="_blank" className="hover:underline block"><Image src="/linkedin.png" className="rounded-2xl border-b-black" alt="logo" width={40} height={40}></Image></a><br />
+          </p>
+          <p className=" mx-2 space-y-2 items-center flex">
+            <a href="https://github.com/alfarabusalihu" target="_blank" className="hover:underline block"><Image src="/github.webp" className="rounded-2xl border-b-black" alt="logo" width={40} height={40}></Image></a><br />
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  </div>
+
+  <div className="w-full md:w-1/2 flex flex-col items-center">
+    <h2 className="text-2xl font-semibold text-gray-800 mb-8">Stacks</h2>
+    <div className="flex border-amber-950 bg-amber-200 rounded-2xl p-4 ">
+    <ul className="grid grid-cols-2 sm:grid-cols-4 gap-3 ">
+      {[
+        "HTML", "CSS", "SCSS", "Tailwind CSS", "Javascript", "Typescript", "Next.js", "Angular Js", "React",
+        "Node Js", "NestJS", "Go", "PHP", "Docker", "Git", "Bootstrap", "Ant Design", "Figma", 
+      ].map((tech) => (
+        <li key={tech} className="bg-amber-950 text-white rounded-2xl px-5 py-3 font-semibold">
+          {tech}
+        </li>
+      ))}
+    </ul>
     </div>
+  </div>
+</main>
   );
 }
